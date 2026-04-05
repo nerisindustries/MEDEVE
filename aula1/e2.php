ceda952e2b950fd5711d2941a93896008fc019ad
DFN <daviferreiraneris@gmail.com>
2026-04-04 23:32:52 -0300

    exercicio 2

--- aula1/e2.php
+++ aula1/e2.php
@@ -1,9 +1,41 @@
-<php
 
+<?php
+$lojinha= [
+[
+"id" => 2,
+"nome" => "Nesquik",
+"valor"=>"470",
+"quantidade" => "10"
+],
+[
+"id" => 3,
+"nome" => "Toddynho",
+"valor" => "220",
+"quantidade" => "8"
+],
+[
+"id" => 4,
+"nome" => "Danoninho",
+"valor" => "120",
+"quantidade" => "5"
+]
 
+];
 
-
-
+echo $lojinha[0]["id"]."<br>";
+echo $lojinha[0]["nome"]."<br>";
+echo $lojinha[0]["valor"]."<br>";   
+echo $lojinha[0]["quantidade"]."<br>";
+echo "-----------------------------<br>";
+echo $lojinha[1]["id"]."<br>";
+echo $lojinha[1]["nome"]."<br>";
+echo $lojinha[1]["valor"]."<br>";   
+echo $lojinha[1]["quantidade"]."<br>";
+echo "-----------------------------<br>";
+echo $lojinha[2]["id"]."<br>";
+echo $lojinha[2]["nome"]."<br>";        
+echo $lojinha[2]["valor"]."<br>";
+echo $lojinha[2]["quantidade"]."<br>";
 
 
 ?>

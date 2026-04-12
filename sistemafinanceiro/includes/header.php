76b6a0ef43a610d743f796252e3e251887c0f73c
nerisindustries <daviferreiraneris@gmail.com>
2026-04-12 16:00:08 -0300

    Exercíco 4

---
+++ sistemafinanceiro/includes/header.php
@@ -0,0 +1,15 @@
+<html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <link href="https://jsdelivr.net" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>sistemmedeve</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+</html>

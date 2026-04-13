f1905c17b978e0fbca11a6ccb5f1369226eaf711
DFN <daviferreiraneris@gmail.com>
2026-04-13 14:40:30 -0300

    aula 6

--- sistemafinanceiro/pages/dashboard.php
+++ sistemafinanceiro/pages/dashboard.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="card text-bg-success">
+<div class="card-body">
+<h5 class="card-title">Contas a Receber</h5>
+<p class="card-text">R$ 1.200,00</p>
+</div>
+</div>
+
+</body>
+</html>
+
+
+
+
+
 <?php
 
 include "../config/config.php";
@@ -14,6 +38,7 @@ elseif ($_SESSION["perfil"] == "empresa") {
 elseif ($_SESSION["perfil"] == "Adimnistrador") {
     echo "<h3>Bem-vindo! Você está no Painel do Adiministrador.</h3>";
 }
+
 
 
 ?>

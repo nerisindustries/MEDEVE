fcf0b8f2510380651c1146216d6e9ce8e4bfe66a
nerisindustries <daviferreiraneris@gmail.com>
2026-04-13 01:31:02 -0300

    Exercíco 5

--- sistemafinanceiro/includes/menu.php
+++ sistemafinanceiro/includes/menu.php
@@ -1,50 +1,38 @@
+
 <?php
-include "../config/config.php";  
-include "../includes/auth.php";  
   
-
-?>
-<?php
-
-
-
-
-
-
-
-$perfil = $_SESSION["perfil"];
+$perfil = $_SESSION["perfil"] ?? "";
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
 <div class="container">
-#Sistema Financeiro</a>
+<a class="navbar-brand" href="dashboard.php">#Sistema Financeiro</a>
 <ul class="navbar-nav ms-auto">
 <li class="nav-item">
-dashboard.phpDashboard</a>
+<a class="nav-link" href="dashboard.php">Dashboard</a>
 </li>
 <?php if ($perfil == "cliente") { ?>
 <li class="nav-item">
-contas/listar.phpMinhas Contas</a>
+<a class="nav-link" href="contas/listar.php">Minhas Contas</a>
 </li>
 <?php } ?>
 <?php if ($perfil == "empresa") { ?>
 <li class="nav-item">
-contas/listar.phpContas da Empresa</a>
+<a class="nav-link" href="contas/listar.php">Contas da Empresa</a>
 </li>
 <li class="nav-item">
-contas/cadastrar.phpCadastrar Conta</a>
+<a class="nav-link" href="contas/cadastrar.php">Cadastrar Conta</a>
 </li>
 <?php } ?>
-
 <?php if ($perfil == "Adimnistrador") { ?>
 <li class="nav-item">
-contas/listar.phpTodas as Contas</a>
+<a class="nav-link" href="contas/listar.php">Todas as Contas</a>
 </li>
 <li class="nav-item">
-usuarios/listar.phpUsuários</a>
+<a class="nav-link" href="usuarios/listar.php">Usuários</a>
 </li>
 <?php } ?>
 <li class="nav-item">
-../logout.phpSair</a>
+<a class="nav-link" href="../logout.php">Sair</a>
 </li>
 </ul>
 </div>

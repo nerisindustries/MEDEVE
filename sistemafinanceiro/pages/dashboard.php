7f60edce5a23038dc69175edd2a5a69018ebba2e
nerisindustries <daviferreiraneris@gmail.com>
2026-04-12 23:00:14 -0300

    aula 4

--- sistemafinanceiro/pages/dashboard.php
+++ sistemafinanceiro/pages/dashboard.php
@@ -1,11 +1,19 @@
 <?php
 
-include "/config/config.php";
-include"/include/auth.php";
+include "../config/config.php";
+include "../includes/auth.php";
 echo"<h1> Seja muito bem vindo," . $_SESSION["usuario"] . "</h1>";
 
 
-
+if ($_SESSION["perfil"] == "cliente") {
+    echo "<h3>Bem-vindo! Esta é a sua Área do Cliente.</h3>";
+} 
+elseif ($_SESSION["perfil"] == "empresa") {
+    echo "<h3>Bem-vindo! Esta é a sua Área da Empresa.</h3>";
+} 
+elseif ($_SESSION["perfil"] == "Adimnistrador") {
+    echo "<h3>Bem-vindo! Você está no Painel do Adiministrador.</h3>";
+}
 
 
 ?>

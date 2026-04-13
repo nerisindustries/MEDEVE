40e41a35c3a7d688ddfdefe0d6b6cfb5191f1d63
nerisindustries <daviferreiraneris@gmail.com>
2026-04-13 00:56:34 -0300

    aula 4

--- sistemafinanceiro/includes/menu.php
+++ sistemafinanceiro/includes/menu.php
@@ -1,24 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
 <div class="container">
 #Sistema Financeiro</a>
-
 <ul class="navbar-nav ms-auto">
 <li class="nav-item">
 dashboard.phpDashboard</a>
 </li>
-
 <?php if ($perfil == "cliente") { ?>
 <li class="nav-item">
-contas/l
-istar.phpMinhas Contas</a>
+contas/listar.phpMinhas Contas</a>
 </li>
-
 <?php } ?>
 <?php if ($perfil == "empresa") { ?>
 <li class="nav-item">
 contas/listar.phpContas da Empresa</a>
 </li>
-
 <li class="nav-item">
 contas/cadastrar.phpCadastrar Conta</a>
 </li>
@@ -31,13 +26,10 @@ contas/listar.phpTodas as Contas</a>
 <li class="nav-item">
 usuarios/listar.phpUsuários</a>
 </li>
-
 <?php } ?>
 <li class="nav-item">
 ../logout.phpSair</a>
 </li>
 </ul>
 </div>
-
-
-?>
+</nav>

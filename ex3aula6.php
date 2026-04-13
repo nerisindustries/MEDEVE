7414af5c61b4fca27e3af7dbe800f9eb0cfc4743
DFN <daviferreiraneris@gmail.com>
2026-04-13 11:15:18 -0300

    aula 6

---
+++ ex3aula6.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+<form>
+<div class="mb-3">
+<label class="form-label">Nome</label>
+<input type="text" class="form-control">
+</div>
+<div class="mb-3">
+<label class="form-label">Email</label>
+<input type="number" class="form-control">
+</div>
+<button class="btn btn-primary" >Enviar</button>
+</form>
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+

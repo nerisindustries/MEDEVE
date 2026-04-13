a51267f4e93b3ac476e20f3d840ce455c4fcf5a4
DFN <daviferreiraneris@gmail.com>
2026-04-13 10:37:21 -0300

    parte 3

---
+++ ex3aula5.php
@@ -0,0 +1,12 @@
+
+<?php
+$perfil= "admin";
+$perfil = "cliente"; // aqui vc troca pra cliente ou admin pra testar 
+if ($perfil == "cliente"){
+    echo "Área do cliente";
+}
+elseif ($perfil == "admin"){
+    echo "Área do adiministrador";
+}
+
+?>

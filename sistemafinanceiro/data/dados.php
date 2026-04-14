7e0a413e4b11d030b03a8a57b424bd6ba61aeac3
DFN <daviferreiraneris@gmail.com>
2026-04-14 14:59:00 -0300

    arrumando

--- sistemafinanceiro/data/dados.php
+++ sistemafinanceiro/data/dados.php
@@ -1,7 +1,7 @@
 <?php
 
 $usuarios = [["usuario" => "cliente", "senha" => "123", "perfil" => "cliente"], ["usuario" => "empresa", "senha" => "123", "perfil" => "empresa"], ["usuario" => "admin", "senha" => "123", "perfil" => "administrador"], ["usuario" => "financas", "senha" => "123", "perfil" => "financeiro"]];
-
+$contas = [["descricao" => "Mensalidade", "valor" => 300, "tipo" => "Pagar"], ["descricao" => "Mensalidade", "valor" => 300, "tipo" => "Pagar"], ["descricao" => "Mensalidade", "valor" => 300, "tipo" => "Pagar"]];
 
 
 
